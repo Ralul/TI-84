f934aa209a879eaf5ce0807bbd10d4a420561e4b
Raoul Messerli <74320658+Ralul@users.noreply.github.com>
2021-12-25 03:51:30 +0100

    1.2

--- Countdown.py
+++ Countdown.py
@@ -1,9 +1,8 @@
 def progressBar(current, total, barLength = 20):
     percent = float(current) * 100 / total
-    arrow   = '-' * int(percent/100 * barLength - 1) + '>'
-    spaces  = ' ' * (barLength - len(arrow))
 
-    print('Progress: [%s%s] %d %%' % (arrow, spaces, percent), end='\r')
+
+    print(percent, end='\r')
 
 i=1
 import time
