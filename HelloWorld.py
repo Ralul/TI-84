d0f52e20f4df978196fb0f1e1613b84c8d87ca13
Raoul Messerli <74320658+Ralul@users.noreply.github.com>
2022-03-18 08:36:36 +0100

    2.0.1

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,9 @@
-print(utf-8("hello world"))
+def add_multiply(zahl1, zahl2):
+    return zahl1+zahl2, zahl1*zahl2
+
+a = add_multiply(4,10)
+print(a, a[0], a[1])
+
+a, b = add_multiply(4,10)
+print(a, b)
+
