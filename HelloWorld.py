3a5da9a8be10928abb077e044d3f8a4c6f98f6eb
Raoul Messerli <74320658+Ralul@users.noreply.github.com>
2022-06-12 16:04:55 +0200

    2.21

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,10 +1,11 @@
-String = input("gib ein")
-Nominal = String
-K = ''
-# loop for all characters
-for ele in String:
-    if ele.isdigit():
-        String = String.replace(ele, K)
-print("The resultant string : " + str(String))
-x = Nominal.rsplit(String)
-print(x)
+x = 0.25
+y = 0.25
+while True:
+    y = y*4
+    x = x+(x/y)
+
+    print (x)
+
+    print (y)
+
+
---
+++ ISO286C.py
@@ -0,0 +1,27 @@
+ES = int(input("ES:"))
+EI = int(input("EI:"))
+
+es = int(input("es"))
+ei = int(input("ei"))
+
+#Spielpassung
+HS = ES - ei
+MS = EI - es
+#Übermasspassung
+HU = es - EI
+MU = ei - ES
+
+if (HS >= 0 and MS >= 0):
+    print("Spielpassung")
+    print ("Höstspiel HS:", HS)
+    print ("Mindestspiel MS:", MS)
+
+if (HU >= 0 and MU >= 0):
+    print("Übermasspassung")
+    print ("Höchstübermass HU:", HU)
+    print ("Mindestübermass MU:", MU)
+
+if (MS >= 0 and MU >= 0):
+    print("Übergagspassung")
+    print ("Höchstübermass MU:", HU)
+    print ("Mindestspiel MS:", MS)
